66a38067e5ff5e2a77945ac51f824837992f8e69
chenxuan <523764720@qq.com>
2017-11-25 02:30:35 +0800

    精简代码，把游戏控制的基本函数丢到gameLib里面，让index主要负责逻辑操作。。类似mvc吧

--- index.js
+++ index.js
@@ -30,98 +30,11 @@ var generationCount= 1;
  */
 var G_deaded = [];
 
-iframeList = [];
-
-/**
- * 操控每一个iframe
- */
-function eachIframe(_do){
-    if(!iframeList){
-        iframeList = document.getElementsByTagName('iframe');
-    }
-    for(var i = 0; i < iframeList.length; i++){
-        _do(iframeList[i].contentWindow, i);
-    }
-}
-
-/**
- * 让对应iframe的小恐龙跳起来
- */
-function pressJump(_win)
-{
-    _win.Runner.instance_.onKeyDown(
-        {
-            keyCode: 38,
-            target: 1
-        }
-    );
-}
-
-function pressDuck(_win)
-{
-    _win.Runner.instance_.onKeyDown(
-        {
-            keyCode: 40,
-            target: 1,
-            preventDefault: function (){}
-        }
-    );
-}
-
-/**
- * 重启某个iframe的小恐龙游戏
- */
-function restart(_win)
-{
-    _win.Runner.instance_.onKeyUp(
-        {
-            type: "mouseup",
-            target: _win.Runner.instance_.canvas,
-            button: 1
-        }
-    );
-}
-
-/**
- * 判断是不是所有iframe的小恐龙游戏都结束了
- */
-function isAllEnd(){
-    var alive = false;
-    eachIframe(function (_win){
-        if(!_win.Runner.instance_.crashed){
-            alive = true;
-        }
-    });
-    return !alive;
-}
-
-/**
- * 获取障碍物的一些属性,要是没有这个障碍物的话,所有属性都返回0
- */
-function getObstaclesAttr(_win, _index, _attrs)
-{
-    var lenOfAttrs = _attrs.length;
-    var attrObj = {};
-    for(var i = 0; i < lenOfAttrs; i++){
-        var theRes = 0;
-        if(typeof _win.Runner.instance_.horizon.obstacles[_index] != typeof UDFUDFUDF){
-            var theObstacle = _win.Runner.instance_.horizon.obstacles[_index];
-            theRes = theObstacle[_attrs[i]];
-        }
-        attrObj[_attrs[i]] = theRes;
-    }
-    return attrObj;
-}
 
 setTimeout(function (){
-    var body = document.getElementsByTagName('body')[0];
-    for(var i = 0; i < G.length; i++){
-        var ifff = document.createElement('iframe');
-        ifff.src="game.html";
-        ifff.frameborder="0";
-        iframeList.push(ifff);
-        body.appendChild(ifff);
-    }
+    
+    createIframe(G.length);
+    
     setTimeout(
         function ()
         {
@@ -166,7 +79,7 @@ setTimeout(function (){
                         /**
                          * 拿障碍物的几个属性
                          */
-                        var obstaclesAttr = getObstaclesAttr(_win, _index, ["xPos", "yPos", "size", "typeConfig", "speedOffset"]);
+                        var obstaclesAttr = getObstaclesAttr(_win, 0, ["xPos", "yPos", "size", "typeConfig", "speedOffset"]);
 
                         /**
                          * 假如障碍物在后面的话,那没必要管了
@@ -229,7 +142,7 @@ setTimeout(function (){
                     setZeroTimeout(arguments.callee);
                 });
 
-            },1000)
+            },500)
             
         },
         10000
